11b6d04dfa961e5bbeded0562a95203b423a4be9
Daniel Józefiuk <daniel.jozefiuk@gmail.com>
2023-04-23 16:31:31 +0200

    admin can adjust hps, and initiative of entities

--- lib/components/entity/entity_admin_view.dart
+++ lib/components/entity/entity_admin_view.dart
@@ -1,6 +1,7 @@
+import 'dart:html';
+
 import 'package:combat_tracker/components/entity/entity.dart';
 import 'package:combat_tracker/shared/widgets/flexible_input_number.dart';
-import 'package:combat_tracker/shared/widgets/input_number.dart';
 import 'package:flutter/material.dart';
 
 class EntityAdminView extends StatefulWidget {
@@ -13,9 +14,40 @@ class EntityAdminView extends StatefulWidget {
 
 class _EntityAdminViewState extends State<EntityAdminView> {
   late final Entity _entity = widget.entity;
-  late final TextEditingController _hpController = TextEditingController(
-    text: _entity.hp.toString(),
-  );
+  late final TextEditingController _hpController = TextEditingController();
+  late final TextEditingController _maxHpController = TextEditingController();
+  late final TextEditingController _initiativeController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _hpController.text = _entity.hp.toString();
+    _maxHpController.text = _entity.maxHp.toString();
+    _initiativeController.text = _entity.initiative.toString();
+
+    _hpController.addListener(_updateHp);
+    _maxHpController.addListener(_updateMaxHp);
+    _initiativeController.addListener(_updateInitiative);
+  }
+
+  _updateHp() {
+    setState(() {
+      _entity.hp = int.parse(_hpController.text == '' ? '0' : _hpController.text);
+    });
+  }
+
+  _updateMaxHp() {
+    setState(() {
+      _entity.maxHp = int.parse(_maxHpController.text == '' ? '0' : _maxHpController.text);
+    });
+  }
+
+  _updateInitiative() {
+    setState(() {
+      _entity.initiative = int.parse(_initiativeController.text == '' ? '0' : _initiativeController.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -25,10 +57,15 @@ class _EntityAdminViewState extends State<EntityAdminView> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            Text(
+              _entity.name,
+              style: const TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(_entity.name),
                 Flexible(
                   child: Stack(
                     children: [
@@ -52,12 +89,13 @@ class _EntityAdminViewState extends State<EntityAdminView> {
                     ],
                   ),
                 ),
-                Text('${_entity.initiative}'),
               ],
             ),
             Row(
               children: [
                 FlexibleInputNumber(controller: _hpController, name: 'Hp'),
+                FlexibleInputNumber(controller: _maxHpController, name: 'Max Hp'),
+                FlexibleInputNumber(controller: _initiativeController, name: 'Initiative'),
               ],
             ),
           ],
